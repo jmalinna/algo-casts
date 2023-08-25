2c8a77aeba9897aa9042cb9c574c623f33d0ba02
Julia Malinina <malinina.artwork@gmail.com>
2023-08-25 22:52:27 +0200

    feat: add 4 solutions in reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+// SOLUTION 1
+//   const splitString = str.split('');
+//   let result = [];
 
+//   for (let i = splitString.length - 1; i >= 0; i -= 1) {
+//     result.push(splitString[i])
+//   }
+
+//   console.log('final result=', result.join(''))
+//   return result.join('');
+
+// SOLUTION 2
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+
+// SOLUTION 3
+//   let reversedString = '';
+//   for (const char of str) {
+//     reversedString = char + reversedString;
+//   }
+//   console.log('reversedString=', reversedString)
+//   return reversedString;
+
+// SOLUTION 4
+  return str.split('').reduce((acc, char) => {
+    console.log('acc=', acc)
+    return char + acc;
+  }, '');
+}
+
+reverse('apple')
 module.exports = reverse;
